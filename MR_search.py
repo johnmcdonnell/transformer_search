cb7ecd751350628708baeaf3d41a0d0c8f5b4949
John V. McDonnell <john.v.mcdonnell@gmail.com>
2022-09-22 22:12:04 -0700

    Generating sentence embeddings for every MR post

--- MR_search.py
+++ MR_search.py
@@ -2,6 +2,11 @@ import os
 import re
 #import html2text
 from bs4 import BeautifulSoup
+import pandas as pd
+import torch
+#import sentence_transformers
+
+output_dir = 'output'
 
 def traverse_wp_articles_dir(root_dir):
     for root, dirs, files in os.walk(root_dir):
@@ -12,6 +17,33 @@ def traverse_wp_articles_dir(root_dir):
             if filename.endswith(".html") and not re.match(r'^\d+\.html$', filename) and not filename=='.html':
                 file_path = os.path.join(root, file)
                 yield file_path
+
+
+def article_to_dict(file_path):
+    with open(file_path, 'r') as f:
+        soup = BeautifulSoup(f, 'html.parser')
+        title = soup.find('h1', {'class': 'entry-title'}).text
+        author = soup.find('span', {'class': 'author'}).text
+        publish_date = soup.find('time', {'class': 'entry-date'}).attrs['datetime']
+
+        content_soup = soup.find('div', {'class': 'entry-content'})
+        content = content_soup.text
+        outlinks = [a.attrs['href'] for a in content_soup.find_all('a', href=True)]
+
+        tags = [a.text for a in soup.find_all('li', {'class': 'text'})]
+        link = soup.find('link').attrs['href']
+        local_path = filename
+
+        return {
+            'title': title,
+            'author': author,
+            'publish_date': publish_date,
+            'content': content,
+            'outlinks': outlinks,
+            'tags': tags,
+            'link': link,
+            'local_path': local_path
+        }
 
 class WP_Article():
     def __init__(self, filename):
@@ -32,13 +64,23 @@ class WP_Article():
         self.link = soup.find('link').attrs['href']
         self.local_path = filename
 
+        self.device = identify_tensor_device()
+        self.initialize_model()
+
     def string_for_model(self):
         formatted_repr = f"""Title: {self.title}
-        Author: {self.author}
-        Content: {self.content}"""
-
+        Tags: {self.tags}
+        Content: {self.content}
+        """
+        
         return formatted_repr
+
+    def initialize_model(self):
+        self.model = sentence_transformers.SentenceTransformer('msmarco-distilbert-dot-v5')
     
+    def get_embeddings(self):
+        pass
+
     def __repr__(self):
         return f'<WP_Article title={self.link}>'
 
@@ -47,10 +89,17 @@ root_dir = '/Users/jvm/Development/web_crawling/marginal_revolution/marginalrevo
 
 articles = []
 for filename in traverse_wp_articles_dir(root_dir):
-    article = WP_Article(filename)
+    article = article_to_dict(filename)
     articles.append(article)
 
-print('Total articles:', len(articles))
+    # Concatenate articles into a pandas Dataframe
+    df = pd.DataFrame(articles)
+
+    # Save to csv
+    df.to_csv(os.path.join(output_dir, 'mr_archive.csv'), index=False)
+
+
+print('Archived articles count:', len(articles))
 #        import pdb; pdb.set_trace()
 
 
---
+++ generate_embeddings.py
@@ -0,0 +1,56 @@
+import numpy as np
+import pandas as pd
+import torch
+import sentence_transformers
+
+def identify_tensor_device():
+    if torch.backends.mps.is_available():
+        device = "mps"
+    elif torch.cuda.is_available():
+        device = "cuda"
+    else:
+        device = "cpu"
+    return device
+
+device = identify_tensor_device()
+
+# Load the model
+#model = sentence_transformers.SentenceTransformer('msmarco-distilbert-base-v4', device=device)
+model = sentence_transformers.SentenceTransformer('all-MiniLM-L12-v2', device=device)
+
+
+
+# Load the dataset
+df = pd.read_csv("output/mr_archive.csv")
+
+# Concatenate title with text
+def concatenate_title_text(row):
+    return f'''Title: {row["title"]}
+    Tags: {', '.join(row["tags"])}
+    Content: {row["content"]}'''
+
+df["concatenated"] = df.apply(concatenate_title_text, axis=1)
+
+# Fetch embeddings for corpus
+list_to_generate = df["concatenated"].tolist()
+embeddings = model.encode(list_to_generate, show_progress_bar=True)
+
+print('Embedding shape:', embeddings.shape)
+
+def test_consistency(first_item_text, embeddings):
+    embeddings_test = model.encode([first_item_text])
+    cos_sims = sentence_transformers.util.cos_sim(embeddings, embeddings_test)
+    
+    try:
+        assert np.argmax(cos_sims) == 0, "First item wasn't the best match with itself"
+    except AssertionError:
+        print(cos_sims)
+
+
+test_consistency(df["concatenated"].head(1)[0], embeddings)
+print('Consitency check passed')
+
+
+# Save the embeddings
+torch.save(embeddings, "output/mr_embeddings.pt")
+
