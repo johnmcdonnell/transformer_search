d4971024c151f6d56fba71765931364cabdf3ee0
John McDonnell <john.v.mcdonnell@gmail.com>
2022-09-29 15:58:53 -0700

    Updates

---
+++ precache-manifest.4bef59572c3f8709f5d645c8ac9b3071.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "c417b5a3f0e45e8fa645",
+    "url": "/semantic-search-demo/static/css/main.06c18569.chunk.css"
+  },
+  {
+    "revision": "c417b5a3f0e45e8fa645",
+    "url": "/semantic-search-demo/static/js/main.226ed02d.chunk.js"
+  },
+  {
+    "revision": "8f08985d83f48d408c48",
+    "url": "/semantic-search-demo/static/js/runtime~main.379913a6.js"
+  },
+  {
+    "revision": "c3524e33077efcc902f3",
+    "url": "/semantic-search-demo/static/js/2.30335405.chunk.js"
+  },
+  {
+    "revision": "ea34b43d1e40f954ba9b",
+    "url": "/semantic-search-demo/static/js/3.3eaac860.chunk.js"
+  },
+  {
+    "revision": "1a3257ca48b7fe180941595bd80ed699",
+    "url": "/semantic-search-demo/index.html"
+  }
+];
